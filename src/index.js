63b4b9f24088222dc240b232cf37522de5c32234
Declan Vea <declanpvea@gmail.com>
2018-05-30 13:36:21 -0400

    consistent color scheme

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import "./index.css";
 import Button from '@material-ui/core/Button';
 
 const style = { 
-    background: 'linear-gradient( 45deg, #FE6B8B 30%, #FF8E53 90% )',
+    background: 'linear-gradient( 45deg, #0D47A1 30%, #A7FFEB 90% )',
     borderRadius: 3,
     border: 0,
     color: 'white',
     height: 48,
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba( 255, 105, 135, .30 )',
  };
  const xSquare = {
    background: '#0D47A1',
